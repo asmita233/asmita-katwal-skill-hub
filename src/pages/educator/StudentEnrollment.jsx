820b6452d45020c772c7f1f741990b23aea5a463
Asmita katwal <asmitakatwal112@gmail.com>
2025-12-12 06:31:35 +0545

    frontend setup and theirpages

---
+++ src/pages/educator/StudentEnrollment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StudentEnrollment = () => {
+  return (
+    <div>
+      <h1>student enroll</h1>
+    </div>
+  )
+}
+
+export default StudentEnrollment
---
+++ src/pages/student/Player.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Player = () => {
+  return (
+    <div>
+        <h1>palyer</h1>
+      
+    </div>
+  )
+}
+
+export default Player
