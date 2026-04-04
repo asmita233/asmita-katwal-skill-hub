00bdeb316d2a392e4953fd87c2167023a74a3fea
Asmita katwal <asmitakatwal112@gmail.com>
2026-04-04 14:21:18 +0545

    improve vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -26,10 +26,7 @@ axios.interceptors.response.use(
   }
 )
 
-// Rendering the main App within the necessary providers:
-// 1. ClerkProvider for authentication management
-// 2. BrowserRouter for handling client-side routing
-// 3. AppContextProvider for shared global application state
+
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <BrowserRouter>
